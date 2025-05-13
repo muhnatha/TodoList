7843184d9e86092a4eed9cb86ed72d968756d6a8
hafizardd <hafizardi4@gmail.com>
2025-05-13 19:25:49 +0700

    fixing structure

--- frontend/app/page.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  // Redirect to login or dashboard based on auth
-  // For now, just send to login
-  redirect('/login');
-}
