bdb067f132d3004cb070c102f4147df58e0130ef
hafizardd <hafizardi4@gmail.com>
2025-05-18 22:26:54 +0700

    fix: add user icon and notification in header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,4 +1,5 @@
 import {React} from 'react';
+import { UserCircleIcon, Bell } from 'lucide-react';
 
 export default function Header({ title }) {
 
@@ -6,6 +7,10 @@ export default function Header({ title }) {
         <>
             <div className={`flex items-center justify-center text-[#232360] ${title.toLowerCase() === 'dashboard' ? 'bg-white' : 'bg-[#FBFAFF]'}`}>
                 <h1 className='text-2xl font-semibold'>{title}</h1>
+                <div className="absolute top-0 right-0 mr-7 mt-5 flex flex-row gap-3">
+                    <Bell className='w-8 h-8 mt-1.5 hover:cursor-pointer text-[#768396]'/>
+                    <UserCircleIcon className='w-10 h-10 hover:cursor-pointer text-[#768396]'/>
+                </div>
             </div>
         </>
     );
